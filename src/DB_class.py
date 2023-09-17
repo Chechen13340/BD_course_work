50d94d3edfc2e43616ed3b4b5788698118c78f5e
Artem Osipov <tema.osipov@mail.ru>
2023-09-17 21:22:41 +0300

    written methods for DB class

--- src/DB_class.py
+++ src/DB_class.py
@@ -1,38 +1,158 @@
-class DBManager:
-    def __init__(self):
-        pass
+from collections import Counter
 
-    def get_companies_and_vacancies_count(self, company_name: list, count_vacancies: int):
+import requests
+
+from settings import HH_API_URL
+
+
+class DBManager:
+    def __init__(self, url, headers, params):
+        self.url = url
+        self.headers = headers
+        self.params = params
+
+    def get_companies_and_vacancies_count(self):
         """
          Метод для получения списка всех компаний
           и количества вакансий у каждой компании.
         """
-        pass
+        employer_information = []
+        for page in range(0, 20):
+            params = {
+                'text': 'мироэлектроника',
+                'area': 113,
+                'per_page': 100,
+                'page': page,
+                'only_with_vacancies': True
+            }
 
-    def get_all_vacancies(self, vacancies_info: list[dict]):
+            url = HH_API_URL
+            headers = {"User-Agent": "HH-User-Agent"}
+            response = requests.get(url, headers=headers, params=params)
+            data = response.json()
+            for num in data['items']:
+                company_name = num['employer']['name']
+
+                if (company_name == 'Радар ММС' or company_name == 'Авангард' or company_name == 'Светлана-Рост'
+                        or company_name == 'Группа компаний Микрон' or company_name == 'Ангстрем'
+                        or company_name == 'ОКБ-ПЛАНЕТА' or company_name == 'НПЦ ЭЛВИС' or company_name == 'НИИ Гириконд'
+                        or company_name == 'Точных Приборов, НИИ' or company_name == 'Российские космические системы'):
+                    employer_information.append(company_name)
+        list_name_count_vacancies = Counter(employer_information)
+        return list_name_count_vacancies
+
+    def get_all_vacancies(self):
         """
         Метод для получения списка всех вакансий с указанием названия компании,
         названия вакансии и зарплаты, и ссылки на вакансию.
         """
-        pass
+        employer_information = []
+        for page in range(0, 20):
+            params = {
+                'text': 'мироэлектроника',
+                'area': 113,
+                'per_page': 100,
+                'page': page,
+                'only_with_vacancies': True
+            }
 
-    def get_avg_salary(self, avg_salary: int):
+            url = HH_API_URL
+            headers = {"User-Agent": "HH-User-Agent"}
+            response = requests.get(url, headers=headers, params=params)
+            data = response.json()
+            for num in data['items']:
+                company_name = num['employer']['name']
+
+                if (company_name == 'Радар ММС' or company_name == 'Авангард' or company_name == 'Светлана-Рост'
+                        or company_name == 'Группа компаний Микрон' or company_name == 'Ангстрем'
+                        or company_name == 'ОКБ-ПЛАНЕТА' or company_name == 'НПЦ ЭЛВИС' or company_name == 'НИИ Гириконд'
+                        or company_name == 'Точных Приборов, НИИ' or company_name == 'Российские космические системы'):
+                    vacancies_name = num['name']
+                    url_vacancies = num['alternate_url']
+                    if num.get('salary') is None:
+                        salary_min = 0
+                        salary_max = 0
+                    elif num['salary']['from'] is None:
+                        salary_min = 0
+                        salary_max = num['salary']['to']
+                    elif num['salary']['to'] is None:
+                        salary_min = num['salary']['from']
+                        salary_max = 0
+                    else:
+                        salary_min = num['salary']['from']
+                        salary_max = num['salary']['to']
+
+                    employer_information.append({
+                        'Название компании': company_name,
+                        'Название вакансии': vacancies_name,
+                        'Заработная плата от': salary_min,
+                        'Заработная плата до': salary_max,
+                        'Ссылка на вакансию': url_vacancies
+                    })
+
+        return employer_information
+
+    def get_avg_salary(self):
         """
         Метод для получения средней
-        зарплатыпо вакансиям.
+        зарплаты по вакансиям.
         """
-        pass
+        avg_salary_all = []
+        for vacancy in self.get_all_vacancies():
+            if vacancy['Заработная плата от'] == 0:
+                avg_salary = vacancy['Заработная плата до']
+            elif vacancy['Заработная плата до'] == 0:
+                avg_salary = vacancy['Заработная плата от']
+            elif vacancy['Заработная плата от'] == 0 and vacancy['Заработная плата до'] == 0:
+                avg_salary = 0
+            else:
+                avg_salary = (vacancy['Заработная плата от'] + vacancy['Заработная плата до']) // 2
+            avg_salary_all.append(avg_salary)
+        return avg_salary_all
 
-    def get_vacancies_with_higher_salary(self, top_vacancies: list):
+    def get_vacancies_with_higher_salary(self):
         """
         Метод для получения списка всех вакансий,
         у которых зарплата выше средней по всем вакансиям.
         """
-        pass
+        higher_salary = []
+        total_salary = 0
+        count = 0
+        for salary in self.get_avg_salary():
+            if salary != 0:
+                total_salary += salary
+                count += 1
+        total_avg_salary = total_salary // count
+        for salary in self.get_all_vacancies():
+            if salary['Заработная плата от'] > total_avg_salary or salary['Заработная плата до'] > total_avg_salary:
+                higher_salary.append(salary)
+        return higher_salary
 
-    def get_vacancies_with_keyword(self, key_word_vacancies: list):
+    def get_vacancies_with_keyword(self, key_word: str):
         """
         Метод для получения списка всех вакансий,
         в названии которых содержатся переданные в метод слова.
         """
-        pass
+        key_word_vacancies = []
+        for vacancy in self.get_all_vacancies():
+            if key_word in vacancy['Название вакансии']:
+                key_word_vacancies.append(vacancy)
+        return key_word_vacancies
+
+
+url = HH_API_URL
+headers = {"User-Agent": "HH-User-Agent"}
+params = {
+    'text': 'мироэлектроника',
+    'area': 113,
+    'per_page': 100,
+    'page': range(0, 20),
+    'only_with_vacancies': True
+}
+i = DBManager(url, headers, params)
+print(i.get_companies_and_vacancies_count())
+print(i.get_all_vacancies())
+print(i.get_avg_salary())
+print(i.get_vacancies_with_higher_salary())
+key_word = input('Введите ключевое слово: ')
+print(i.get_vacancies_with_keyword(key_word))
