8dd16ae012e38bf30938292d2ded1f7c376c1884
Artem Osipov <tema.osipov@mail.ru>
2023-09-18 19:56:47 +0300

    added main py file for start programm

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+from config import config
 from settings import HH_API_URL
-from src.DB_class import DBManager
+from DB_class import DBManager
+from utils import create_database, save_data_to_database
 
 
 def main():
     url = HH_API_URL
     headers = {"User-Agent": "HH-User-Agent"}
-    params = {
+    param = {
         'text': 'мироэлектроника',
         'area': 113,
         'per_page': 100,
         'page': range(0, 20),
         'only_with_vacancies': True
     }
-    i = DBManager(url, headers, params)
-    print(i.get_companies_and_vacancies_count())
-    print(i.get_all_vacancies())
-    print(i.get_avg_salary())
-    print(i.get_vacancies_with_higher_salary())
+    db_headhunter = DBManager(url, headers, param)
+    print(db_headhunter.get_companies_and_vacancies_count())
+    print(db_headhunter.get_all_vacancies())
+    print(db_headhunter.get_avg_salary())
+    print(db_headhunter.get_vacancies_with_higher_salary())
     key_word = input('Введите ключевое слово: ')
-    print(i.get_vacancies_with_keyword(key_word))
+    print(db_headhunter.get_vacancies_with_keyword(key_word))
+    params = config()
+    create_database('headhunter', params)
+    save_data_to_database(db_headhunter.get_all_vacancies(), 'headhunter', params)
+
 
 if __name__ == '__main__':
     main()
