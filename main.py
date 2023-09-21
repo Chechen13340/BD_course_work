1e3980836488e7bac7efc60ca2aa6cdf637ef6de
Artem Osipov <tema.osipov@mail.ru>
2023-09-21 09:40:25 +0300

    fixed main py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from config import config
-from settings import HH_API_URL
 from DB_class import DBManager
 from utils import create_database, save_data_to_database
 
 
 def main():
-
     params = config()
     create_database('headhunter', params)
     save_data_to_database('headhunter', params)
@@ -16,8 +14,6 @@ def main():
     print(db_headhunter.get_vacancies_with_higher_salary())
     key_word = input('Введите ключевое слово: ')
     print(db_headhunter.get_vacancies_with_keyword(key_word))
-
-
 
 
 if __name__ == '__main__':
